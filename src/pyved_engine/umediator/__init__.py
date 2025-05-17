ef621ae09e1987f51bb9a05015f4ef976e53c369
wkta <thomas.iw@kata.games>
2025-05-17 16:08:06 +0200

    (docs/code readability):better comments for pyv.umediator

--- src/pyved_engine/umediator/__init__.py
+++ src/pyved_engine/umediator/__init__.py
@@ -4,13 +4,14 @@ As of time of writing (March 2025),
 
 The goal in the long-run is to:
 - totally abstract away the transport layer for network comms/networking
-- allow the mediator class (also used in pyv.actors)
- to handle the client-server architecture+communication,
- but without any "overhead" for the game dev
 
-Overheard meaning? In the computing Context: extra resources (like time or memory) needed to manage or execute
-a process beyond the core tasks
+- allow the mediator class (also used in pyv.actors)
+ to automatically process all [client server architecture]-based
+  comms, therefore hiding "overhead"/complexities from the game dev
+
+Overheard meaning what? In the computing Context:
+extra resources (like time or memory) needed to manage or execute a process beyond the core tasks
 """
 from .UMediator import UMediator
-from .netlayer_factory import *  # the build(a1, a2) function, and also the Objectifier cls
 from .helpers import EventReadyCls
+from .netlayer_factory import *  # imports the specific .build(a1, a2) function. Also contains the Objectifier cls
